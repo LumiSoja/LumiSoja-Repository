018473344630eb82129017308e78f291d17220be
GabrielGPL101 <gabriel.plima@sptech.school>
2026-09-23 12:28:23 -0300

    Codigo parametrizado

--- Sprint2/Arquitetura_computacional/dat-acqu-ino/main.js
+++ Sprint2/Arquitetura_computacional/dat-acqu-ino/main.js
@@ -50,9 +50,9 @@ const serial = async (
     // processa os dados recebidos do Arduino
     arduino.pipe(new serialport.ReadlineParser({ delimiter: '\r\n' })).on('data', async (data) => {
         console.log(data);
-        const valores = data.split(';');
-        const sensorDigital = parseInt(valores[0]);
-        const sensorAnalogico = parseFloat(valores[1]);
+        // const valores = data.split(';');
+        const sensorAnalogico = parseFloat(valores[0]);
+        // const sensorDigital = parseFloat(valores[1]);
 
         // armazena os valores dos sensores nos arrays correspondentes
         valoresSensorAnalogico.push(sensorAnalogico);
